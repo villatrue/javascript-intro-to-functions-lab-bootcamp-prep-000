126d0e7ffa7c79fea7a5007ef9afca8804150a2a
Sean Carter <scarter0313@gmail.com>
2019-01-03 04:04:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string) {
     return "YES INDEED!"
   }
   else if (string === "I love you, Grandma."){
-    return "I love you, too"
+    return "I love you, too."
   }
 }
 
